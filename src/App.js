1f766db0cafdafede2d1dadc7e5a4c8b8965ae59
NeginShirmahalle <negin.shirmahalle@gmail.com>
2023-08-27 00:51:34 +0330

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Weather App
+      </header>
+      <footer> Coded by <a href=''>Negin</a></footer>
+    </div>
+  );
+}
+
+export default App;
